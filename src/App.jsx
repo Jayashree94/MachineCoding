9d5f28f923972184f8fe7d183d5feed21f8643ca
Jayashree <s.jayashree94@gmail.com>
2025-07-22 08:23:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ToDo from './Components/ToDo.jsx'
+
+const App = () => {
+  return (
+  <div class='bg-stone-900 grid py-4 min-h-screen'>
+      <ToDo/>
+    </div>
+   
+  )
+}
+
+export default App
