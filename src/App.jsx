fc766de3e11f354d68d3390b635d901c54b148f9
Jayashree <s.jayashree94@gmail.com>
2025-07-26 07:25:54 +0530

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,54 @@
-import { useState, useEffect } from "react";
-
-export default function App() {
+import React from "react";
+import { useEffect, useState } from "react";
+import "./app.css";
+const App = () => {
   const [products, setProducts] = useState([]);
+  const [currPage, setcurrPage] = useState(0);
 
-  const fetchProducts = async () => {
-    const data = await fetch("https://dummyjson.com/products?limit=100"); // Add valid URL here
-    const json = await data.json();
-
-    if (json && json.products) {
-      setProducts(json.products);
-    }
+  const Productdets = ({ image, title }) => {
+    return (
+      <div className="product_card">
+        <img src={image} alt="thumbmail"></img>
+        <span> {title}</span>
+      </div>
+    );
   };
 
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/products?limit=200");
+    const json = await data.json();
+    console.log(json);
+    setProducts(json.products);
+  };
+
+  const PAGE_SIZE = 20;
+  const totalProducts = products.length;
+  const noOfPages = Math.ceil(totalProducts / PAGE_SIZE);
+
   useEffect(() => {
-    fetchProducts();
+    fetchData();
   }, []);
 
-  return (
-    <div className="products">
-      <div class="bg-white dark:bg-black text-black dark:text-white">
-        Dark mode enabled
-        <h1>hello</h1>
+  return !products.length ? (
+    <h1> No products Found</h1>
+  ) : (
+    <div className="App">
+      <h1>Pagination</h1>
+      
+      <div className="product_list">
+        {products.slice(0,10).map((p) => {
+          return <Productdets key={p.id} image={p.thumbnail} title={p.title} />;
+        })}
       </div>
-      {products.map((p) => {
-       return <span key={p.id}>{p.title}</span>
-})}
+
+      <div className="Page">
+        {[...Array(noOfPages).keys()].map((n) => (
+          <span  className = "page-num" key={n}> {n} </span>
+        ))}
+      </div>
+      
     </div>
-  )
-}
+  );
+};
+
+export default App;
